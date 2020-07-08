510a9cfc70786aff03c0465641e04a9f7ec3ae74
Jaffer  Syed <jaffersyed@Jaffers-MacBook-Pro.local>
2020-07-08 15:47:29 -0400

    Updates

---
+++ precache-manifest.217457f202f3c95038abc2d10a5ad279.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b8ae56f6b0e7a630ab32da2c589e2c",
+    "url": "/recover-website/index.html"
+  },
+  {
+    "revision": "66fbd6dab67dcc347340",
+    "url": "/recover-website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a47114c5eb133f581ba9",
+    "url": "/recover-website/static/css/main.236bf607.chunk.css"
+  },
+  {
+    "revision": "66fbd6dab67dcc347340",
+    "url": "/recover-website/static/js/2.fc4c89b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recover-website/static/js/2.fc4c89b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47114c5eb133f581ba9",
+    "url": "/recover-website/static/js/main.d906b90b.chunk.js"
+  },
+  {
+    "revision": "6f05c53b0c98362b8bd0",
+    "url": "/recover-website/static/js/runtime-main.68fe6df7.js"
+  },
+  {
+    "revision": "29031a03619fd6ca850da8d28fb941ac",
+    "url": "/recover-website/static/media/background.29031a03.png"
+  },
+  {
+    "revision": "109cd5878494e78e87799c4df99737fa",
+    "url": "/recover-website/static/media/background2.109cd587.png"
+  },
+  {
+    "revision": "aa0ec2803b70769a84bfc031e5bda9b4",
+    "url": "/recover-website/static/media/intervoguereg.aa0ec280.otf"
+  },
+  {
+    "revision": "fbf7a1006126c408fdd78117a1aadedd",
+    "url": "/recover-website/static/media/intervoguesoftblack.fbf7a100.otf"
+  },
+  {
+    "revision": "e4907bfb7a35b45af2a2daea6bf30988",
+    "url": "/recover-website/static/media/intervoguesoftmed.e4907bfb.otf"
+  }
+]);
